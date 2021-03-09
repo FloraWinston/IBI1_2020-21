a6f31bda8ff8ac1053defd6f4eb2f6a5ce10332c
3200111044bit <3200111044@zju.edu.cn>
2021-03-09 15:14:22 +0000

    a little change to fibonacci sequence

--- Practical5/collatz.py
+++ Practical5/collatz.py
@@ -5,7 +5,7 @@ print(a); print(b)
 # use a for-loop to do addition
 # totally will add 11 times for values
 for i in range(1,12):
-# define x to do the addition
+    # define x to do the addition
     x = a + b
     # redefine a, b when the loop is continuing
     b = a
