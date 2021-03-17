f83f9aa4801a1c42cdcf9aa4dbbcb1e212d5dd8c
3200111044bit <3200111044@zju.edu.cn>
2021-03-17 02:52:45 +0100

    changes made to List_manipulation.py

--- Practical6/List_manipulation.py
+++ Practical6/List_manipulation.py
@@ -14,20 +14,27 @@ for i in range(len(gene_lengths)):
     a = gene_lengths[i]/exon_counts[i]
     # add a to the list used for storage
     average_exon_length.append(a)
-# print out the sorted list anddo not change the previous list
+# print out the sorted list and do not change the previous list
 print(sorted(average_exon_length))
 
 # draw a boxplot
 # These codes are adapted from the lecture PowerPoint, thanks for providing these useful examples.
+# And I also look for online sources to make further adaptation. 
+
 # import the library for pie chart
 # Note: only import matplotlib is nut enough for making boxplot
-import matplotlib.pyplot as py
+import matplotlib.pyplot as plt 
 # modity the boxplot
-py.boxplot(average_exon_length, vert = True, whis = 1.5, widths = 1.5, meanline = True, showmeans = True, 
-           labels =['The average of exon length'],)
+# Note1 vert is used to place the boxplot vertically.
+# Note2 whis is used to determine whisker
+# Note3 widths is used to decide the width of the boxplot
+# Note4 meanline and showmeans is used to adjust the existance of mean value.
+# Note5 patch_artist is used to adjuat the appearance of boxplot (also cooperate with last five functions
+# Note6 labels indicates the label on the boxplot. 
+plt.boxplot(average_exon_length, vert = True, whis = 1.5, widths = 0.3, patch_artist = True, meanline = True, showmeans = True, 
+           labels = ['The average of exon length'], boxprops = {'facecolor' : 'lightblue', 'color' : 'grey'}, 
+           flierprops = {'marker':'o'}, capprops = {'color':'grey', 'markersize':4},
+           meanprops = {'markerfacecolor':'green','markersize':4},
+		   medianprops = {'linestyle':'-','color':'orange'})
 # To show user the boxplot
-py.show()
-
-
-
-
+plt.show()
